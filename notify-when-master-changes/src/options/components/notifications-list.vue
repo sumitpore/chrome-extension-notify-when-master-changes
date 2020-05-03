4b13d9bd6d8fe4c03ee972f0bc03b558579e8eeb
Sumit Pore <sumit.pore@gmail.com>
2020-05-03 17:13:28 +0530

    Complete UI that shows Notifications

--- notify-when-master-changes/src/options/components/notifications-list.vue
+++ notify-when-master-changes/src/options/components/notifications-list.vue
@@ -2,27 +2,37 @@
   <div class="container">
     <h1>Notifications</h1>
     <section v-if="!emptyNotificationList">
-      <div v-for="(repoNotifications, repoIdentifier) in notifications" :key="repoIdentifier" class="repo-item">
+      <div v-for="(commits, repoIdentifier) in notifications" :key="repoIdentifier" class="repo-item">
         <div class="repo-title">
           <input type="checkbox" v-model="selectedRepos" :value="repoIdentifier" />
           <a :href="getRepoUrlFromIdentifier(repoIdentifier)" target="_blank">{{ repoIdentifier }}</a>
         </div>
 
-        <div class="notifications">
-          <a
-            v-for="notification in repoNotifications"
-            :key="notification.sha"
-            :title="notification.message"
-            :href="`https://github.com/${repoIdentifier}/commit/${notification.sha}`"
-            >{{ notification.sha.substring(0, 7) }}</a
-          >
+        <div class="commits-wrapper">
+          <button
+            v-if="commits.length > minCommitsCountForCarousel"
+            class="left-arrow-icon"
+            :id="`${repoIdentifier}-previous-commits-icon`"
+            @click="showPreviousCommits(repoIdentifier)"
+          ></button>
+          <div class="commits" :id="`${repoIdentifier}-commits`">
+            <a v-for="commit in commits" :key="commit.sha" :title="commit.message" :href="`https://github.com/${repoIdentifier}/commit/${commit.sha}`">{{
+              commit.sha.substring(0, 7)
+            }}</a>
+          </div>
+          <button
+            v-if="commits.length > minCommitsCountForCarousel"
+            class="right-arrow-icon"
+            :id="`${repoIdentifier}-next-commits-icon`"
+            @click="showNextCommits(repoIdentifier)"
+          ></button>
         </div>
 
         <div class="action-items">
-          <a href @click.prevent="markRepoNotificationsRead" :key="repoIdentifier">Mark Read</a>
           <button class="mute-icon" title="Unsubscribe" @click="unsubscribeRepo(repoIdentifier)"></button>
         </div>
       </div>
+      <button class="primary-btn" :disabled="selectedRepos.length == 0" @click="markSelectedReposNotificationRead">Mark Read</button>
     </section>
 
     <section v-else>
@@ -38,9 +48,15 @@ export default {
   name: 'NotificationsList',
 
   mixins: [mixin],
-
+  // 'getredash/redash': [
+  //   {
+  //     sha: '175aefa0b85ed9a740636f816495479e57870d9d',
+  //     message: 'minor fixes',
+  //   },
+  // ],
   data: function() {
     return {
+      minCommitsCountForCarousel: 3,
       notifications: {
         'getredash/redash': [
           {
@@ -49,22 +65,98 @@ export default {
           },
         ],
       },
+      singleCommitElementWidth: 0,
     };
   },
 
-  created: function() {
-    let dummyNotificationsRepo = 'getredash/redash';
-    let sampleObj = {
-      sha: '175aefa0b85ed9a740636f816495479e57870d9d',
-      message: 'minor fixes',
-    };
-    this.notifications[dummyNotificationsRepo].push(sampleObj, sampleObj, sampleObj, sampleObj, sampleObj, sampleObj, sampleObj, sampleObj);
+  mounted: async function() {
+    if (this.emptyNotificationList) {
+      return;
+    }
+
+    await this.$nextTick();
+
+    let singleCommitElement = document.querySelector('.commits a:first-of-type');
+    if (singleCommitElement != null) {
+      this.singleCommitElementWidth = singleCommitElement.clientWidth;
+    }
+
+    this.controlVisibilityPreviousAndNextIcons();
   },
 
   computed: {
     emptyNotificationList: function() {
       return Object.keys(this.notifications).length === 0;
     },
+  },
+
+  methods: {
+    showPreviousCommits: function(repoIdentifier) {
+      let commitsElement = document.getElementById(`${repoIdentifier}-commits`);
+      commitsElement.scrollLeft = commitsElement.scrollLeft - commitsElement.offsetWidth + this.singleCommitElementWidth;
+    },
+
+    showNextCommits: function(repoIdentifier) {
+      let commitsElement = document.getElementById(`${repoIdentifier}-commits`);
+      commitsElement.scrollLeft = commitsElement.scrollLeft + commitsElement.offsetWidth - this.singleCommitElementWidth;
+    },
+
+    controlVisibilityPreviousAndNextIcons: function() {
+      let allCommitsElements = document.querySelectorAll('.commits');
+      if (allCommitsElements == null) return;
+      for (let commitsElement of allCommitsElements) {
+        if (commitsElement.childElementCount <= this.minCommitsCountForCarousel) return;
+        let commitsWrapper = commitsElement.closest('.commits-wrapper');
+        this.addObserverOnFirstCommitVisibilityOfRepo(commitsElement, commitsWrapper);
+        this.addObserverOnLastCommitVisibilityOfRepo(commitsElement, commitsWrapper);
+      }
+    },
+
+    addObserverOnFirstCommitVisibilityOfRepo: function(commitsElement, commitsWrapper) {
+      let previousCommitsIconElement = commitsWrapper.querySelector('.left-arrow-icon');
+      let firstCommitElement = commitsElement.querySelector('a:first-of-type');
+      let firstCommitObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              previousCommitsIconElement.hidden = true;
+            } else {
+              previousCommitsIconElement.hidden = false;
+            }
+          });
+        },
+        {
+          root: commitsElement,
+          rootMargin: '0px 0px 0px 10px',
+          threshold: 1.0,
+        }
+      );
+      firstCommitObserver.observe(firstCommitElement);
+    },
+
+    addObserverOnLastCommitVisibilityOfRepo: function(commitsElement, commitsWrapper) {
+      let nextCommitsIconElement = commitsWrapper.querySelector('.right-arrow-icon');
+      let lastCommitElement = commitsElement.querySelector('a:last-of-type');
+      let lastCommitObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              nextCommitsIconElement.hidden = true;
+            } else {
+              nextCommitsIconElement.hidden = false;
+            }
+          });
+        },
+        {
+          root: commitsElement,
+          rootMargin: '0px 10px 0px 0px',
+          threshold: 1.0,
+        }
+      );
+      lastCommitObserver.observe(lastCommitElement);
+    },
+
+    markSelectedReposNotificationRead: function() {},
   },
 };
 </script>
@@ -80,42 +172,55 @@ export default {
   }
 }
 
-.notifications {
-  width: 35%;
-  overflow-x: scroll;
-}
-
 .action-items {
-  width: 10%;
+  width: 12%;
   text-align: right;
   margin-right: 30px;
 
   a {
     margin-right: 8px;
+    text-decoration: none;
+    color: var(--primary-color);
+    vertical-align: middle;
   }
 }
 
-.notifications {
+.commits-wrapper {
+  width: 33%;
+  display: flex;
+  align-items: center;
+}
+
+.commits {
+  overflow-x: scroll;
+  white-space: nowrap;
+  border-radius: 3px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+  margin-left: 20px;
+  margin-right: 20px;
+  scroll-behavior: smooth;
+
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    background: transparent;
+  }
+
   a {
-    @include allSidesCollapsedBorder(1px, var(--primary-border-color));
     padding: 5px;
     text-decoration: none;
     color: var(--primary-color);
     background: white;
-
+    display: inline-block;
+    border-right: 1px solid var(--primary-border-color);
+    font-size: 16px;
     &:hover {
       background: var(--primary-color);
       color: white;
     }
 
-    &:first-of-type {
-      border-top-left-radius: 5px;
-      border-bottom-left-radius: 5px;
-    }
-
     &:last-of-type {
-      border-top-right-radius: 5px;
-      border-bottom-right-radius: 5px;
+      border-right: 0;
     }
   }
 }
