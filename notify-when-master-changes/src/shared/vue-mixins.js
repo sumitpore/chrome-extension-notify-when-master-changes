8d6eccac0895c94914d489a0f589a68ef0664c00
Sumit Pore <sumit.pore@gmail.com>
2020-05-13 09:50:46 +0530

    ESLint Fix

--- notify-when-master-changes/src/shared/vue-mixins.js
+++ notify-when-master-changes/src/shared/vue-mixins.js
@@ -2,14 +2,14 @@ import { getRepoUrlFromIdentifier } from '../utils';
 import { deleteRepoInfoFromStorage } from '../data-layer/repo-info-storage-api';
 
 export default {
-  data: function() {
+  data() {
     return {
       selectedRepos: [],
     };
   },
   methods: {
     getRepoUrlFromIdentifier,
-    unsubscribeRepo: function(repoIdentifier) {
+    unsubscribeRepo(repoIdentifier) {
       deleteRepoInfoFromStorage(repoIdentifier);
       if (typeof this.savedRepos[repoIdentifier] == 'undefined') return;
       this.$delete(this.savedRepos, repoIdentifier);
