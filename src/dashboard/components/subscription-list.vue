7588a57eb584e6c8ce91a994f44a73ac09bfcda5
Sumit Pore <sumit.pore@gmail.com>
2020-05-17 10:14:31 +0530

    Fix indentation

--- src/dashboard/components/subscription-list.vue
+++ src/dashboard/components/subscription-list.vue
@@ -11,16 +11,10 @@
           <button class="mute-icon" title="Unsubscribe" @click="unsubscribeRepo(repoIdentifier)"></button>
         </div>
       </div>
-      <button
-        class="primary-btn"
-        :disabled="selectedRepos.length == 0"
-        @click="unsubscribeSelectedRepos"
-      >Unsubscribe</button>
+      <button class="primary-btn" :disabled="selectedRepos.length == 0" @click="unsubscribeSelectedRepos">Unsubscribe</button>
     </section>
     <section v-else>
-      <div
-        class="repo-item no-items"
-      >Visit Github Repository of your choice and click 'Subscribe to Master' button.</div>
+      <div class="repo-item no-items">Visit Github Repository of your choice and click 'Subscribe to Master' button.</div>
     </section>
   </div>
 </template>
